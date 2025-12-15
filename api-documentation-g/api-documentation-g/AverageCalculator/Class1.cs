646b208a47d6bdc786cf2433cf7b3d2acece2a33
Student <foobar@sr.htlweiz.at>
2025-12-15 10:25:18 +0100

    First_steps

---
+++ api-documentation-g/api-documentation-g/AverageCalculator/Class1.cs
@@ -0,0 +1,21 @@
+﻿namespace Api;
+
+public class AverageCalculator
+{
+    private double[] collection = {0};
+    private Int64 count = 0;
+    public void add(double value)
+    {
+        collection[count] = value;
+        count++;
+    }
+    public void add(double[] values)
+    {
+        Int64 i = 0;
+        while (i > values.Length)
+        {
+            add(values[i]);
+            i++;
+        }
+    }
+}

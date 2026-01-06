eaf823901272558546c97a5c8106a574f5e26ca9
Student <foobar@sr.htlweiz.at>
2026-01-06 15:19:55 +0100

    second_step

---
+++ AverageCalculator/AverageCalculator.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Reflection.Metadata.Ecma335;
+
+namespace AverageCalculator;
+
+public class AverageCalculator
+{
+    private double[] collection = { 0 };
+    private Int64 count = 0;
+
+    public AverageCalculator()
+    {
+
+    }
+    public void add(double value)
+    {
+        collection[count] = value;
+        count++;
+    }
+    public void add(double[] values)
+    {
+        double? test_null = values[0];
+        if (!test_null.HasValue)
+        {
+            Console.WriteLine("Bitte ein Array mit Werten!");
+        }
+        else
+        {
+            Int64 i = 0;
+            while (i > values.Length)
+            {
+                add(values[i]);
+                i++;
+            }  
+        }
+        
+    }
+    public void getAverage()
+    {
+        if (collection[0] == double.NaN)
+        {
+            
+        }
+    }
+
+}
---
+++ Program/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Program;
+using AverageCalculator;
+class Program {
+
+    public static int Main()
+    {
+        double test_value = 5;
+        AverageCalculator test = new AverageCalculator();
+        test.add(test_value);
+        Console.Writeline("hello");
+        return 0;
+    }
+}
